7a9183f81509d89c9a6d860e04a5bf9be3325823
edukey <erwan.ducroquet2@gmail.com>
2026-07-26 13:01:05 +0200

    enh

--- webxr/tjs_basic.js
+++ webxr/tjs_basic.js
@@ -1,26 +1,40 @@
 import * as THREE from 'https://threejs.org/build/three.module.js'
-import { VRButton } from 'https://threejs.org/examples/jsm/webxr/VRButton.js'
+// import { VRButton } from 'https://threejs.org/examples/jsm/webxr/VRButton.js'
+
+let LOG = null
 
 init()
 
-async function init() {
-	console.log('init')
+function myLog(txt) {
+  console.log(txt);
+  if (LOG) LOG.innerText = txt + "\n" + LOG.innerText;
+}
+
+
+async function sceneFromProject(url) {
+  myLog(`Loading ${url} ...`);
+  const r = await fetch(url);
+  if (r.status != 200) {
+    myLog(`Failed to load ${url} : ${r.status}`);
+    return null
+  }
+  const project = await r.json();
+  myLog("Project loaded and parsed");
+  myLog("Init THREE.ObjectLoader");
+  const ldr = new THREE.ObjectLoader();
+  try {
+    const scene = await ldr.parseAsync(project.scene);
+    return scene
+  } catch (e) {
+    myLog(`Parsing scene from ${url} FAILED : ${e}`);
+  }
+  return null;
+}
+
+function sceneBasic() {
 	const scene = new THREE.Scene()
 	console.log('Scene:', scene)
-
-	const vp_width = 500
-	const vp_height = 500
 	
-	const camera = new THREE.PerspectiveCamera( 50, vp_width/vp_height, 0.1, 10 )
-	camera.position.set( 0, 1.75, 0 )
-	scene.add( camera )
-
-	// scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) )
-	
-	const light = new THREE.DirectionalLight( 0xffffff )
-	light.position.set( 1, 1, 1 ).normalize()
-	scene.add( light )
-
 	// --- 1. Define 3D Objects via ThreeJs : geom, material, mesh
 	const mprm = { // THREE.MeshStandardMaterialParameters
 		roughness: 0.5,
@@ -33,44 +47,70 @@ async function init() {
 	sphereMesh.position.set(0, 1.75, -3);
 	scene.add(sphereMesh);
 
+	return scene	
+}
+
+async function init() {
+	// HTML
+	// document.body.appendChild( VRButton.createButton( renderer ) )
+	const button = document.createElement( 'button' )
+	button.textContent = 'XR ?';
+	document.body.appendChild(button)
+	LOG = document.createElement( 'pre' )
+	document.body.appendChild(LOG)
+	document.body.appendChild(document.createElement( 'br' ))
+
+	myLog('init')
+
+	const scene = await sceneFromProject("iwsdk/project.json")
+	// const scene = sceneBasic()
+
+	const camera = new THREE.PerspectiveCamera( 50, 1, 0.1, 10 )
+	camera.position.set( 0, 1.75, 0 )
+	scene.add( camera )
+
+	scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) )
+	
+	const light = new THREE.DirectionalLight( 0xffffff )
+	light.position.set( 1, 1, 1 ).normalize()
+	scene.add( light )
+
+	const vp_width = 500
+	const vp_height = 500
+
 	const renderer = new THREE.WebGLRenderer( { antialias: true } )
 	renderer.setPixelRatio( window.devicePixelRatio )
 	renderer.setSize( vp_width, vp_height )
 	renderer.outputEncoding = THREE.sRGBEncoding
 	renderer.setAnimationLoop(()=>{renderer.render( scene, camera )})
 	renderer.xr.enabled = true
-	console.log('Render', scene)
+	myLog('Renderer done')
 
-	const info = document.createElement( 'span' )
-	info.innerText = 'Bla bla'
-	const button = document.createElement( 'button' )
-	button.textContent = 'XR ?';
-	document.body.appendChild( VRButton.createButton( renderer ) )
-	document.body.appendChild(button)
-	document.body.appendChild(info)
-	document.body.appendChild(document.createElement( 'br' ))
 	document.body.appendChild( renderer.domElement )
 
 	if(!('xr' in navigator )) {
+		myLog('No XR')
 		button.textContent = 'No XR';
 		return
 	}
 	const ok = await navigator.xr.isSessionSupported( 'immersive-vr' )
 	if(!ok) {
+		myLog('VR session not supported')
 		button.textContent = 'VR session not supported';
 		return
 	}
 
 	button.textContent = 'Enter XR';
-	button.onclick = ()=>{
-		navigator.xr.requestSession( 'immersive-vr').then( (session)=>{
-			console.log('VR session started', session)
-			session.onend= ()=> {
-				console.log('VR session ended')
-				button.textContent = 'ENTER VR';
+	button.onclick = () => {
+		myLog('Entering VR ...')
+		// setting features is mandatory else WebXRManager.setSession NotSupportedError: The requested reference space type is not supported.
+		const opts = { optionalFeatures: [ 'local-floor', 'bounded-floor', 'layers' ] }
+		navigator.xr.requestSession('immersive-vr', opts).then( (session)=>{
+			myLog('VR session started')
+			session.onend = ()=> {
+				myLog('VR session ended')
 			}
-			button.textContent = 'EXIT VR';
-			console.log('Associating WebXR session to THREEjs renderer', session)
+			myLog('Associating WebXR session to THREEjs renderer')
 			renderer.xr.setSession( session ) // async
 		} )
 	}
