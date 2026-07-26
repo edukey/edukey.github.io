7ccfdf83a3db6e21c48392ba239b438f6589c84b
edukey <erwan.ducroquet2@gmail.com>
2026-07-26 23:49:57 +0200

    enh

--- webxr/tjs_basic.js
+++ webxr/tjs_basic.js
@@ -13,27 +13,27 @@ let HUD = null
 init()
 
 
-function myLog(txt) {
+function log(txt) {
   console.log(txt);
   if (LOG) LOG.innerText = txt + "\n" + LOG.innerText;
 }
 
 async function sceneFromProject(url) {
-  myLog(`Loading ${url} ...`);
+  log(`Loading ${url} ...`);
   const r = await fetch(url);
   if (r.status != 200) {
-    myLog(`Failed to load ${url} : ${r.status}`);
+    log(`Failed to load ${url} : ${r.status}`);
     return null
   }
   const project = await r.json();
-  myLog("Project loaded and parsed");
-  myLog("Init THREE.ObjectLoader");
+  log("Project loaded and parsed");
+  log("Init THREE.ObjectLoader");
   const ldr = new THREE.ObjectLoader();
   try {
     const scene = await ldr.parseAsync(project.scene);
     return scene
   } catch (e) {
-    myLog(`Parsing scene from ${url} FAILED : ${e}`);
+    log(`Parsing scene from ${url} FAILED : ${e}`);
   }
   return null;
 }
@@ -58,36 +58,36 @@ function sceneBasic() {
 }
 
 /** Controllers button presses events high level */
-function initCtrl(xr) {
+function ctrlHighInit(xr) {
 	// events from xrController : (dis)connect (select/squeeze)(/start/end)
 	// but no events on other items : use the Gamepad API during render to test the status of 
 	//   buttons[] 0:trig 1:grip 2:. 3:thumbPress 4:A/X 5:B/Y
 	//   axis[] 0:X 1:Y for thumbstick
 	const c0=xr.getController( 0 ) // Left
-	c0.addEventListener( 'selectstart', ()=>{moveUser(xr, -1)})
+	c0.addEventListener( 'selectstart', ()=>{userMove(xr, -1)})
 	// c0.addEventListener( 'selectend', ()=>{})
-	c0.addEventListener( 'squeezestart', ()=>{moveUser(xr, -0.1)})
+	c0.addEventListener( 'squeezestart', ()=>{userMove(xr, -0.1)})
 	// c0.addEventListener( 'squeezeend', ()=>{})
 
 	const c1=xr.getController( 1 ) // Right
-	c1.addEventListener( 'selectstart', ()=>{moveUser(xr, 1)})
+	c1.addEventListener( 'selectstart', ()=>{userMove(xr, 1)})
 	// c1.addEventListener( 'selectend', ()=>{})
-	c1.addEventListener( 'squeezestart', ()=>{moveUser(xr, 0.1)})
+	c1.addEventListener( 'squeezestart', ()=>{userMove(xr, 0.1)})
 	// c1.addEventListener( 'squeezeend', ()=>{})
 }
 
 /** apply a transform to move the player
  * https://threejs.org/docs/#WebXRManager.getReferenceSpace
  * */
-function chgRefByOffsetTransfo(xr, offsetTransfo) {
+function userOffsetTransfo(xr, offsetTransfo) {
   xr.setReferenceSpace(xr.getReferenceSpace().getOffsetReferenceSpace(offsetTransfo));
 }
 
-function teleportOffsetXZ(xr, delta_x, delta_z) {
-	chgRefByOffsetTransfo(xr, new XRRigidTransform({ x: -delta_x, y: 0, z: -delta_z, w: 1 }))
+function userTeleportOffsetXZ(xr, delta_x, delta_z) {
+	userOffsetTransfo(xr, new XRRigidTransform({ x: -delta_x, y: 0, z: -delta_z, w: 1 }))
 }
 
-function rotateUser(xr, angleDeg) {
+function userRotate(xr, angleDeg) {
 	const radian = THREE.MathUtils.degToRad(angleDeg)
 	const rotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), radian)
 	const transform = new XRRigidTransform({}, // no translation
@@ -95,14 +95,14 @@ function rotateUser(xr, angleDeg) {
         x: 0,
         y: rotation.y,
         z: 0,
-        w: rotation.w // should be 1 ?
+        w: 1 // rotation.w // should be 1 ?
     }
 	)
 	console.log("rotate", angleDeg, radian, rotation.y, rotation.w)
-	chgRefByOffsetTransfo(xr, transform)
+	userOffsetTransfo(xr, transform)
 }
 
-function deltaUser(xr, dist) {
+function userDelta(xr, dist) {
 	const xrCamera = xr.getCamera();
 
 	// 2. Extract the forward direction vector
@@ -121,21 +121,21 @@ function deltaUser(xr, dist) {
 }
 
 /** slide perpendicular to the direction of the camera */
-function slideUser(xr, dist) {
+function userSlide(xr, dist) {
 	console.log("slide", dist)
-	const delta = deltaUser(xr, dist)
+	const delta = userDelta(xr, dist)
 	// we just exchange z and x
-	teleportOffsetXZ(xr, delta.z, delta.x)
+	userTeleportOffsetXZ(xr, delta.z, delta.x)
 }
 
 /** walk using the direction of the camera */
-function moveUser(xr, dist) {
+function userMove(xr, dist) {
 	console.log("move", dist)
-	const delta = deltaUser(xr, dist)
-	teleportOffsetXZ(xr, delta.x, delta.z)
+	const delta = userDelta(xr, dist)
+	userTeleportOffsetXZ(xr, delta.x, delta.z)
 }
 
-function setHud(hud, a, b, c) {
+function hudSet(hud, a, b, c) {
 	if(!hud) return
 	hud.texture.needsUpdate = true // tell the texture to update from canvas
   hud.ctx.clearRect(0, 0, hud.canvas.width, hud.canvas.height)
@@ -145,7 +145,7 @@ function setHud(hud, a, b, c) {
 }
 
 /** create a HUD Canvas to draw text to */
-function initHud(camera) {
+function hudInit(camera) {
 		const hud = { }
 		const charPix = 15
     hud.canvas = document.createElement('canvas');
@@ -177,8 +177,8 @@ async function init() {
 	document.body.appendChild(LOG)
 	document.body.appendChild(document.createElement( 'br' ))
 
-	const VERSION = '--- D ---'
-	myLog(`init version ${VERSION}`)
+	const VERSION = '--- E ---'
+	log(`init version ${VERSION}`)
 
 	const scene = await sceneFromProject("iwsdk/project.json")
 	// const scene = sceneBasic()
@@ -187,8 +187,8 @@ async function init() {
 	camera.position.set( 0, 1.75, 4 )
 	scene.add( camera )
 
-	HUD = initHud(camera)
-	setHud(HUD, '',VERSION,'')
+	HUD = hudInit(camera)
+	hudSet(HUD, '',VERSION,'')
 
 	// above the scene, with color fading from the sky color to the ground color. no shadows https://threejs.org/docs/#HemisphereLight 
 	scene.add( new THREE.HemisphereLight() )
@@ -219,64 +219,64 @@ async function init() {
 
 	const my_controllers = {
 		firstError: null,
-		right: initCtrlLow('right',{
-			left:()=>{rotateUser(renderer.xr,-45)},
-			right:()=>{rotateUser(renderer.xr,45)},
-			trig:()=>{moveUser(renderer.xr, 1)},
-			grip:()=>{moveUser(renderer.xr, -1)},
-			by:()=>{moveUser(renderer.xr, 0.1)},
-			ax:()=>{moveUser(renderer.xr, -0.1)}
+		right: ctrlInit('right',{
+			left:()=>{userRotate(renderer.xr,-45)},
+			right:()=>{userRotate(renderer.xr,45)},
+			trig:()=>{userMove(renderer.xr, 1)},
+			grip:()=>{userMove(renderer.xr, -1)},
+			by:()=>{userMove(renderer.xr, 0.1)},
+			ax:()=>{userMove(renderer.xr, -0.1)}
 		}),
-		left: initCtrlLow('left',{
-			trig:()=>{moveUser(renderer.xr, 0.1)},
-			grip:()=>{moveUser(renderer.xr, -0.1)},
-			up:()=>{moveUser(renderer.xr, 0.1)},
-			down:()=>{moveUser(renderer.xr, -0.1)},
-			left:()=>{slideUser(renderer.xr,-1)},
-			right:()=>{slideUser(renderer.xr,1)}
+		left: ctrlInit('left',{
+			trig:()=>{userSlide(renderer.xr, -0.1)},
+			grip:()=>{userSlide(renderer.xr, 0.1)},
+			up:()=>{userMove(renderer.xr, 0.1)},
+			down:()=>{userMove(renderer.xr, -0.1)},
+			left:(c)=>{userSlide(renderer.xr,c.grip.pressed?0.1:1)},
+			right:(c)=>{userSlide(renderer.xr,c.grip.pressed?-0.1:-1)}
 		})
 	}
 	// initCtrl(renderer.xr)
 
 	renderer.setAnimationLoop(()=>{
 		//statsVR.update()
-		loopCtrlLow(renderer.xr, my_controllers)
+		ctrlLoop(renderer.xr, my_controllers)
 		renderer.render( scene, camera )
 	})
 
-	myLog('Renderer done')
+	log('Renderer done')
 
 	document.body.appendChild( renderer.domElement )
 
 	if(!('xr' in navigator )) {
-		myLog('No XR')
+		log('No XR')
 		button.textContent = 'No XR';
 		return
 	}
 	const ok = await navigator.xr.isSessionSupported( 'immersive-vr' )
 	if(!ok) {
-		myLog('VR session not supported')
+		log('VR session not supported')
 		button.textContent = 'VR session not supported';
 		return
 	}
 
 	button.textContent = 'Enter XR';
 	button.onclick = () => {
-		myLog('Entering VR ...')
+		log('Entering VR ...')
 		// setting features is mandatory else WebXRManager.setSession NotSupportedError: The requested reference space type is not supported.
 		const opts = { optionalFeatures: [ 'local-floor', 'bounded-floor', 'layers' ] }
 		navigator.xr.requestSession('immersive-vr', opts).then( (session)=>{
-			myLog('VR session started')
+			log('VR session started')
 			session.onend = ()=> {
-				myLog('... VR session ended')
+				log('... VR session ended')
 			}
-			myLog('Start THREEjs XR on WebXR session ...')
+			log('Start THREEjs XR on WebXR session ...')
 			renderer.xr.setSession( session ) // async
 		} )
 	}
 }
 
-function initCtrlLow(name, cbs) {
+function ctrlInit(name, cbs) {
 	if(!cbs) cbs={}
 	return {
 		name: name,
@@ -294,78 +294,78 @@ function initCtrlLow(name, cbs) {
 	}
 }
 
-function evtCtrlLowPressed(c, obj, pressed, info) {
+function ctrlCheckPressAction(c, obj, pressed, info) {
 	// if(!obj.cb) return
 	if(!obj.pressed && pressed) {
 		// myLog('Pressed '+info+' '+c.name)
-		setHud(HUD, 'Pressed', info+' '+c.name)
+		hudSet(HUD, 'Pressed', info+' '+c.name)
 		obj.pressed=true
-		if(obj.cb) obj.cb()
+		if(obj.cb) obj.cb(c)
 	}
 	if(obj.pressed && !pressed) {
 		obj.pressed=false
 	}
 }
 
-function errCtrlLow(errObj, where, e) {
+function ctrlErr(errObj, where, e) {
 	if(!errObj) return
 	if(errObj.firstError) return
 	try {
 		errObj.firstError = `Error in ${where} : ${e}`
-		myLog(errObj.firstError)
-		setHud(HUD, "ERROR", "loopCtrlLow", e)
+		log(errObj.firstError)
+		hudSet(HUD, "ERROR", "loopCtrlLow", e)
 	}
 	catch(_e2){
 		1
 	}
 }
 
-function evtCtrlLow(c, g) {
+function ctrlEvent(myCtrl, gamepad) {
 	// https://developer.mozilla.org/en-US/docs/Web/API/Gamepad
-	if(!c) return
-	if(!g) return // possible if XR session not started yet
-	if(!g.buttons)  { errCtrlLow(c, 'evtCtrlLow '+c.name, 'No buttons'); return }
-	if(!g.buttons.length)	{ errCtrlLow(c, 'evtCtrlLow '+c.name, 'Empty buttons array'); return }
+	if(!myCtrl) return
+	if(!gamepad) return // possible if XR session not started yet
+	if(!gamepad.buttons)  { ctrlErr(myCtrl, 'ctrlEvent '+myCtrl.name, 'No buttons'); return }
+	if(!gamepad.buttons.length)	{ ctrlErr(myCtrl, 'ctrlEvent '+myCtrl.name, 'Empty buttons array'); return }
 
-	// if(!c.firstMsg) { myLog(`${c.name} first evtCtrlLow ...`) }
+	// if(!c.firstMsg) { myLog(`${c.name} first ctrlEvent ...`) }
 
   // const _trig = g.buttons[0]
   // const _grip = g.buttons[1]  		
-	evtCtrlLowPressed(c, c.trig, g.buttons[0]?.pressed, "TRIG")
-	evtCtrlLowPressed(c, c.grip, g.buttons[1]?.pressed, "GRIP")
-	evtCtrlLowPressed(c, c.thb, g.buttons[3]?.pressed, "THUMB")
-	evtCtrlLowPressed(c, c.ax, g.buttons[4]?.pressed, "A/X")
-	evtCtrlLowPressed(c, c.by, g.buttons[5]?.pressed, "B/Y")
+	ctrlCheckPressAction(myCtrl, myCtrl.trig, gamepad.buttons[0]?.pressed, "TRIG")
+	ctrlCheckPressAction(myCtrl, myCtrl.grip, gamepad.buttons[1]?.pressed, "GRIP")
+	ctrlCheckPressAction(myCtrl, myCtrl.thb, gamepad.buttons[3]?.pressed, "THUMB")
+	ctrlCheckPressAction(myCtrl, myCtrl.ax, gamepad.buttons[4]?.pressed, "A/X")
+	ctrlCheckPressAction(myCtrl, myCtrl.by, gamepad.buttons[5]?.pressed, "B/Y")
 
-	if(!g.axes) { errCtrlLow(c, 'evtCtrlLow '+c.name, 'No axes'); return }
-	if(!g.axes.length) { errCtrlLow(c, 'evtCtrlLow '+c.name, 'Empty axes array'); return }
-	if(g.axes.length<2) { errCtrlLow(c, 'evtCtrlLow '+c.name, 'Not at leat two axes, but: '+g.axes.length); return }
-	evtCtrlLowPressed(c, c.left, g.axes.length>2 && g.axes[2]<-0.95, "THB X Left")
-	evtCtrlLowPressed(c, c.right, g.axes.length>2 && g.axes[2]>0.95, "THB X Right")
-	evtCtrlLowPressed(c, c.up, g.axes.length>3 && g.axes[3]<-0.95, "THB Y Up")
-	evtCtrlLowPressed(c, c.down, g.axes.length>3 && g.axes[3]>0.95, "THB Y Down")
+	if(!gamepad.axes) { ctrlErr(myCtrl, 'ctrlEvent '+myCtrl.name, 'No axes'); return }
+	if(!gamepad.axes.length) { ctrlErr(myCtrl, 'ctrlEvent '+myCtrl.name, 'Empty axes array'); return }
+	if(gamepad.axes.length<2) { ctrlErr(myCtrl, 'ctrlEvent '+myCtrl.name, 'Not at leat two axes, but: '+gamepad.axes.length); return }
+	ctrlCheckPressAction(myCtrl, myCtrl.left, gamepad.axes.length>2 && gamepad.axes[2]<-0.95, "THB X Left")
+	ctrlCheckPressAction(myCtrl, myCtrl.right, gamepad.axes.length>2 && gamepad.axes[2]>0.95, "THB X Right")
+	ctrlCheckPressAction(myCtrl, myCtrl.up, gamepad.axes.length>3 && gamepad.axes[3]<-0.95, "THB Y Up")
+	ctrlCheckPressAction(myCtrl, myCtrl.down, gamepad.axes.length>3 && gamepad.axes[3]>0.95, "THB Y Down")
 	// if(!c.firstMsg) {
-	//  	myLog(`${c.name} first evtCtrlLow axes: ${g.hand} ${g.axes.length} ${g.axes[0]} ${g.axes[1]} ${g.axes[2]} ${g.axes[3]}`)
+	//  	myLog(`${c.name} first ctrlEvent axes: ${g.hand} ${g.axes.length} ${g.axes[0]} ${g.axes[1]} ${g.axes[2]} ${g.axes[3]}`)
 	//  	c.firstMsg = true
 	// }
 }
 
-function loopCtrlLow(xr, my_controllers) {
+function ctrlLoop(xr, myControllers) {
 	try {
 		const session = xr.getSession();
 		if (!session) return
 		// https://developer.mozilla.org/en-US/docs/Web/API/XRInputSource
 	  const inputSources = session.inputSources;
 		// if(!my_controllers.firstMsg) {
-		// 	myLog(`first loopCtrlLow , INPUTS : ${inputSources?inputSources.length:'NONE !'}`)
+		// 	myLog(`first ctrlLoop , INPUTS : ${inputSources?inputSources.length:'NONE !'}`)
 		// 	my_controllers.firstMsg = true
 		// }
 
-	  evtCtrlLow(my_controllers.left, inputSources[0]?.gamepad)
-	  evtCtrlLow(my_controllers.right, inputSources[1]?.gamepad)
+	  ctrlEvent(myControllers.left, inputSources[0]?.gamepad)
+	  ctrlEvent(myControllers.right, inputSources[1]?.gamepad)
 	  return
 	}
 	catch(e) {
-		errCtrlLow(my_controllers, 'loopCtrlLow', e)
+		ctrlErr(myControllers, 'ctrlLoop', e)
 	}
 }
